bd7c938d522bc6afc5d336f39d47ba3eb1bc25fd
Samin Ul Alam <48744887+Samin-043@users.noreply.github.com>
2021-03-17 15:26:02 +0600

    Update SQLQueryWMS.sql

--- SQLQueryWMS.sql
+++ SQLQueryWMS.sql
@@ -2,31 +2,21 @@ use WarehouseManagementSystem
 
 create table supplier
 (
-	supplier_id int primary key,  
+	supplier_id	int identity(1000,1) primary key,  
 	name varchar(15) not null,
 	address_sup varchar(50) not null,
 	phone_num varchar(11) not null,
-	email varchar(15) not null 
+	email varchar(30) not null 
 )
 
+drop table supplier
+
 select * from supplier
-
-insert into supplier(supplier_id,name,address_sup,phone_num,email) 
-values(1000,'Karim','Savar,Dhaka','01725557771','karim@gmail.com'),
-	(1001,'Rahim','Tilagor,Sylhet','01555555522','rahim@gmail.com'),
-	(1002,'Akbar','Townhall,Comilla','01776668883','akbar@gmail.com')
-	
-insert into supplier(supplier_id,name,address_sup,phone_num,email) 
-values (1003,'Asif','Kuakata,Patuakhali','01991117874','asif@yahoo.com')
-
-insert into supplier(supplier_id,name,address_sup,phone_num,email) 
-values (1004,'Akib','Aftabnagar,Dhaka','01993335646','akib@mail.com')
-
 
 
 create table product
 (
-	product_id int primary key,
+	product_id int identity(1,1) primary key,
 	product_name varchar(50) not null,
 	product_quantity numeric(5,1) not null,
 	product_unit varchar(20) not null,
@@ -36,27 +26,29 @@ create table product
 	supplier_id int not null foreign key references supplier(supplier_id) 
 )
 
-/*drop table product*/
+drop table product
 select * from product
 
-insert into product(product_id,product_name,product_quantity,product_unit,product_price,product_description,category,supplier_id)
-values (1,'Miniket Rice',5,'kg',60,'Better quality from past','Rice',1000),
-		(2,'Orange',4,'kg',170,'important for vitamin C','Fruits',1001),
-		(3,'Aarong Full Cream Liquid UHT Milk',500,'ml',45,'Good for health','Milk & Dairy Products',1002)
+insert into product(product_name,product_quantity,product_unit,product_price,product_description,category,supplier_id)
+values ('Miniket Rice',5,'kg',60,'Better quality from past','Rice',1000),
+		('Orange',4,'kg',170,'important for vitamin C','Fruits',1001),
+		('Aarong Full Cream Liquid UHT Milk',500,'ml',45,'Good for health','Milk & Dairy Products',1002)
 
 
 create table inventory
 (
-	inventory_id int primary key,
+	inventory_id int identity(5000,1) primary key,
 	product_id int not null foreign key references product(product_id)
 )
 
+drop table inventory
+
 select * from inventory
 
-insert into inventory(inventory_id,product_id)
-values(5000,1),
-	(5001,2),
-	(5002,3)
+insert into inventory(product_id)
+values(2),
+	(3),
+	(4)
 
 
 
@@ -69,7 +61,7 @@ create table customer
 	email varchar(50) not null
 )
 
-
+drop table customer
 select * from customer
 
 insert into customer(name,customer_address,phone_no,email)
@@ -81,24 +73,24 @@ values('Ameer','Paribagh','01721469854','ameer@gmail.com'),
 
 create table payment
 (
-	bill_number int primary key,
+	bill_number int identity(12388,1) primary key,
     customer_id int not null foreign key references customer(customer_id),
 	payment_type varchar(50) not null,
-	payment_date date null,
+	payment_date date null
 )
 
-
+drop table payment
 select * from payment
 
-insert into payment(bill_number,customer_id,payment_type,payment_date) 
-values(12388,3000,'cash','02-15-2021'),
-	(12389,3001,'cash','02-21-2021'),
-	(12390,3002,'Credit Card','03-01-2021')
+insert into payment(customer_id,payment_type,payment_date) 
+values(3000,'cash','02-15-2021'),
+	(3001,'cash','02-21-2021'),
+	(3002,'Credit Card','03-01-2021')
 
 
 create table staff
 (
-	staff_id int primary key,
+	staff_id int identity(8000,1) primary key,
 	name varchar(15) not null,
 	staff_address varchar(50) not null,
 	phone_no varchar(11) not null,
@@ -107,19 +99,20 @@ create table staff
 	customer_id int not null foreign key references customer(customer_id)
 )
 
+drop table staff
 select * from staff
 
-insert into staff(staff_id,name,staff_address,phone_no,email,staff_title,customer_id)
-values(8000,'Jalal','Gulshan','01745628938','jalal@gmail.com','Manager',3000),
-	  (8001,'Mojnu','Banani','01735628459','mojnu@gmail.com','Employee',3001),
-	  (8002,'Kabir','Badda','01835528957','kabir@gmail.com','Employee',3002)
+insert into staff(name,staff_address,phone_no,email,staff_title,customer_id)
+values('Jalal','Gulshan','01745628938','jalal@gmail.com','Manager',3000),
+	  ('Mojnu','Banani','01735628459','mojnu@gmail.com','Employee',3001),
+	  ('Kabir','Badda','01835528957','kabir@gmail.com','Employee',3002)
 
 
 
 
 create table order_details
 (
-	order_details_id int primary key, 
+	order_details_id int identity(10,1) primary key, 
 	product_id int not null foreign key references product(product_id),
 	customer_id int not null foreign key references customer(customer_id),
 	staff_id int not null foreign key references staff (staff_id),
@@ -128,14 +121,11 @@ create table order_details
 	discount varchar(20) not null
 )
 
+drop table order_details
 
 select * from order_details
 
-insert into order_details(order_details_id,product_id,customer_id,staff_id,bill_number,order_date,discount) 
-values(10,1,3000,8000,12388,'02-15-2021','30%'),
-	(11,2,3001,8001,12389,'02-21-2021','22%'),
-	(12,3,3002,8002,12390,'03-01-2021','25%')
-
-
-
-
+insert into order_details(product_id,customer_id,staff_id,bill_number,order_date,discount) 
+values(2,3000,8000,12388,'02-15-2021','30%'),
+	(3,3001,8001,12389,'02-21-2021','22%'),
+	(4,3002,8002,12390,'03-01-2021','25%')
